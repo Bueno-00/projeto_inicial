282d0ea2749494d607d7fface391ca53f092ddb0
Rafael Bueno <rafabuenode@gmailcom>
2025-08-15 20:50:00 -0300

    todos commitados

---
+++ Aulas_faculdade/main.py
@@ -0,0 +1,4 @@
+idade = 35
+nome = "Rafael Bueno"
+
+print(f'Olá meu nome é {nome} e tenho {idade} de idade.')
---
+++ GetCommodities.py
@@ -0,0 +1,8 @@
+import yfinance as yf # pandas e request já estão importados nessa biblioteca
+
+ticker = yf.Ticker("GC=F")
+
+df = ticker.history(period="1d", interval="1m")[['Close']]
+
+print(df.tail())
+
